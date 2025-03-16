87fb076e0116397c00debd40d2a24a23beb77755
TimLai666 <43640816+TimLai666@users.noreply.github.com>
2025-03-16 15:30:18 +0800

    Update email.go

--- lib/email.go
+++ lib/email.go
@@ -1,6 +1,29 @@
 package lib
 
+import (
+	"bytes"
+	"net/http"
+)
+
 func SendEmailVerifyCode(email string, code string) error {
 	// todo
+	mailStruct := struct {
+		Recipient string `json:"recipient"`
+		Type      string `json:"type"`
+		Content   string `json:"content"`
+	}{
+		Recipient: email,
+		Type:      "verify",
+		Content:   code,
+	}
+	milJson, err := json.Marshal(mailStruct)
+	if err != nil {
+		return err
+	}
+	res, err := http.Post("http://sendemail/SendEmail", "application/json", bytes.NewBuffer(milJson))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
 	return nil
 }
